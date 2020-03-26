5e1b3966eaa137a63d30043eebd82ce0b385d739
Pyry Halonen <halnpy@live.fi>
2020-03-26 19:48:28 +0200

    kurssitiedot valmis

--- osa1/kurssitiedot/kurssiedot/src/index.js
+++ osa1/kurssitiedot/kurssiedot/src/index.js
@@ -3,56 +3,67 @@ import ReactDOM from 'react-dom'
 
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
-      <Header course={course} />
-      <Content exercises1 = {exercises1} part1 = {part1} 
-                exercises2 = {exercises2} part2 = {part2} 
-                exercises3 = {exercises3} part3 = {part3} />
-      <Total exercises1 = {exercises1} exercises2 = {exercises2} 
-              exercises3 = {exercises3} />
-
-
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   )
 }
 
-const Header = (muuttuja) => {
+const Header = (props) => {
   return (
     <div>
-      <h1>{muuttuja.course}</h1>
+      <h1>{props.course}</h1>
     </div>
   )
 }
 
-const Content = (muuttuja) => {
-  return(
+const Part = (props) => {
+  return (
     <div>
-      <p>{muuttuja.part1} {muuttuja.exercises1}</p>
-      <p>{muuttuja.part2} {muuttuja.exercises2}</p>
-      <p>{muuttuja.part3} {muuttuja.exercises3}</p>
-      
+      <p>{props.name} {props.exercises}</p>
+    </div>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <div>
+      <Part name={props.parts[0].name} exercises={props.parts[0].exercises} />
+      <Part name={props.parts[1].name} exercises={props.parts[1].exercises} />
+      <Part name={props.parts[2].name} exercises={props.parts[2].exercises} />
     </div>
   )
 
 }
 
 const Total = (props) => {
-  return(
+  return (
     <div>
-      <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+      <p>Number of exercises {props.parts[0].exercises
+        + props.parts[1].exercises + props.parts[2].exercises}</p>
     </div>
   )
 }
-
-
 
 ReactDOM.render(<App />, document.getElementById('root'))
