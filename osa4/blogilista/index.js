f264e3acf5af3bd696ad743d7cdbf4b072be5f5c
Pyry Halonen <halonpy@live.fi>
2020-04-16 21:06:26 +0300

    4.2 valmis

--- osa4/blogilista/index.js
+++ osa4/blogilista/index.js
@@ -1,47 +1,10 @@
+const app = require('./app')
 const http = require('http')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
+const config = require('./utils/config')
+const Blog = require('./models/blog')
 
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
+const server = http.createServer(app)
 
-const Blog = mongoose.model('Blog', blogSchema)
-
-const mongoUrl = 'mongodb+srv://fullstack:salis33@cluster0-9mhwz.mongodb.net/blogilista?retryWrites=true&w=majority'
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true})
-
-app.use(cors())
-app.use(express.json())
-
-app.get('/', (request, response) => {
-    response.send('<h1> KAK </h1>')
-  })
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
-
-const PORT = 3003
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+server.listen(config.PORT, () => {
+    console.log(`Server running on port ${config.PORT}`)
 })
