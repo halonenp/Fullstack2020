0404ed6c4812b39bfbabe606077dc3c97f91a7b2
Pyry Halonen <halonpy@live.fi>
2020-05-07 17:57:35 +0300

    6.2 valmis

--- osa6/unicafe-redux/src/index.js
+++ osa6/unicafe-redux/src/index.js
@@ -11,16 +11,49 @@ const App = () => {
       type: 'GOOD'
     })
   }
+  const ok = () => {
+    store.dispatch({
+      type: 'OK'
+    })
+  }
+  const bad = () => {
+    store.dispatch({
+      type: 'BAD'
+    })
+  }
+  const zero = () => {
+    store.dispatch({
+      type: 'ZERO'
+    })
+  }
+
+  const all = store.getState().good + store.getState().ok + store.getState().bad
+  const average = (store.getState().good + ((-1) * store.getState().bad)) / all || 0
+  const positive = (store.getState().good / all) * 100 || 0
 
   return (
     <div>
-      <button onClick={good}>good</button> 
-      <button>neutral</button> 
-      <button>bad</button>
-      <button>reset stats</button>
-      <div>good {store.getState().good}</div>
-      <div>neutral</div>
-      <div>bad</div>
+      <button onClick={good}>
+        hyvä
+        </button>
+      <button onClick={ok}>
+        neutraali
+        </button>
+      <button onClick={bad}>
+        huono
+        </button>
+      <button onClick={zero}>
+        nollaa
+        </button>
+      <h3>statistics</h3>
+      {all !== 0 ? <>
+        <div>hyvä {store.getState().good}</div>
+        <div>neutraali {store.getState().ok}</div>
+        <div>huono {store.getState().bad}</div>
+        <div>keskiarvo {average}</div>
+        <div>positiiviset {positive}%</div>
+      </> :
+        <div>no feedback given </div>}
     </div>
   )
 }
